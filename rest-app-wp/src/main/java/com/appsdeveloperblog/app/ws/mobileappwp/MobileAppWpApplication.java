5e1e756ae55379eef42c0a8c6482523a99fb28f4
Tolo1002 <44027170+Tolo1002@users.noreply.github.com>
2018-10-10 15:01:54 +0200

    Add files via upload
    Test

---
+++ rest-app-wp/src/main/java/com/appsdeveloperblog/app/ws/mobileappwp/MobileAppWpApplication.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.app.ws.mobileappwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWpApplication.class, args);
+	}
+}
